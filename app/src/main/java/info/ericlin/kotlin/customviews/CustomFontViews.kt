0b4901498720b4b7295cb8585c2f2e59e1596c40
Eric Lin <nohitme@gmail.com>
2017-06-12 21:29:32 -0500

    Implement custom font views

---
+++ app/src/main/java/info/ericlin/kotlin/customviews/CustomFontViews.kt
@@ -0,0 +1,51 @@
+package info.ericlin.kotlin.customviews
+
+import android.content.Context
+import android.graphics.Typeface
+import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.AppCompatEditText
+import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
+
+/**
+ * Your custom logic to read custom typefaces or attributes
+ */
+private fun readTypeFace(context: Context, attrs: AttributeSet?): Typeface? {
+    attrs ?: return null
+    val a = context.theme.obtainStyledAttributes(attrs, R.styleable.CustomFont, 0, 0)
+    return try {
+        val fontName = a.getString(R.styleable.CustomFont_font_name)
+        Typeface.createFromAsset(context.assets, fontName)
+    } catch(e: Exception) {
+        null
+    } finally {
+        a?.recycle()
+    }
+}
+
+class CustomFontTextView @JvmOverloads constructor(context: Context,
+                                                   attrs: AttributeSet? = null,
+                                                   defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+    init {
+        typeface = readTypeFace(context, attrs)
+    }
+}
+
+class CustomFontButton @JvmOverloads constructor(context: Context,
+                                                 attrs: AttributeSet? = null,
+                                                 defStyleAttr: Int = 0
+) : AppCompatButton(context, attrs, defStyleAttr) {
+    init {
+        typeface = readTypeFace(context, attrs)
+    }
+}
+
+class CustomFontEditText @JvmOverloads constructor(context: Context,
+                                                   attrs: AttributeSet? = null,
+                                                   defStyleAttr: Int = 0
+) : AppCompatEditText(context, attrs, defStyleAttr) {
+    init {
+        typeface = readTypeFace(context, attrs)
+    }
+}
